afaa41a5c4d08fa6064105bfc0e61e5c923cf757
Wambuingare <wambuinancyngare@gmail.com>
2022-05-08 14:30:39 +0300

    changes

--- app/src/main/java/com/example/myapplication/MoneyActivityApp.kt
+++ app/src/main/java/com/example/myapplication/MoneyActivityApp.kt
@@ -1,11 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MoneyActivityApp : AppCompatActivity() {
+    lateinit var btnCancel:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_money_app)
+        btnCancel=findViewById(R.id.btnCancel)
+        btnCancel.setOnClickListener {
+            val intent= Intent(this,MoneyActivityApp::class.java)
+            startActivity(intent)
+        }
     }
 }
